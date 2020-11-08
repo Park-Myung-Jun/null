b808b4ad40f73c63441b62bcc241992cde6e32de
박명준 <juneblack@naver.com>
2020-11-09 05:39:40 +0900

    event plus component

--- life_coding/src/comps/TOC.js
+++ life_coding/src/comps/TOC.js
@@ -7,7 +7,23 @@ class TOC extends Component {
         var data = this.props.data;
         var i = 0;
         while (i < data.length) {
-            list.push(<li key={data[i].id}><a href={"/content/" + data[i].id}>{data[i].title}</a></li>);
+            list.push(<li key={data[i].id}>
+                <a href={"/content/" + data[i].id}
+                    data-id={data[i].id} //data-id에서 dataset의 id를 통해 접근 가능
+                    onClick={function (e) {
+                        e.preventDefault();
+                        this.props.onChangePage(e.target.dataset.id);
+                    }.bind(this)}>
+                    {data[i].title}
+                </a>-or-
+                <a href={"/content/" + data[i].id}
+                    data-id={data[i].id} //data-id에서 dataset의 id를 통해 접근 가능
+                    onClick={function (id, e) {
+                        e.preventDefault();
+                        this.props.onChangePage(id);
+                    }.bind(this, data[i].id)}>
+                    {data[i].title}
+                </a></li>);
             i = i + 1;
         }
         return (
