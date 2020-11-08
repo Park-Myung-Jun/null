64bb77d46c387f78a6c6e1ea7e6d72e23659b7ba
박명준 <juneblack@naver.com>
2020-11-09 04:51:13 +0900

    state

--- life_coding/src/App.js
+++ life_coding/src/App.js
@@ -4,12 +4,27 @@ import TOC from "./comps/TOC";
 import Content from "./comps/Content"
 
 class App extends Component {
+  //init
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: 'WEB', sub: 'world wide web!' },
+      contents: [
+        { id: 1, title: 'HTML', desc: 'HTML is for information' },
+        { id: 2, title: 'CSS', desc: 'CSS is for design' },
+        { id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive' },
+      ],
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}>
+        </Subject>
         <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
       </div>
     );
