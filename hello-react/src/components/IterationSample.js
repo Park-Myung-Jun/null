17d4ab61bfaee2d1b1da77825c75fd94b08810e1
박명준 <juneblack@naver.com>
2020-11-17 17:46:20 +0900

    arrow keycode

--- hello-react/src/components/IterationSample.js
+++ hello-react/src/components/IterationSample.js
@@ -32,6 +32,21 @@ const IteratorSample = () => {
         }
     }
 
+    const onKeyDown = e => {
+        if (e.keyCode === 38) {
+            console.log(38)// up arrow
+        }
+        else if (e.keyCode === 40) {
+            console.log(40)// down arrow
+        }
+        else if (e.keyCode === 37) {
+            console.log(37)// left arrow
+        }
+        else if (e.keyCode === 39) {
+            console.log(39)// right arrow
+        }
+    }
+
     const nameList = names.map(name => (
         <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
             {name.text}
@@ -39,7 +54,7 @@ const IteratorSample = () => {
     ));
     return (
         <>
-            <input value={inpuText} onChange={onChange} onKeyPress={onKeyPress}/>
+            <input value={inpuText} onChange={onChange} onKeyPress={onKeyPress} onKeyDown={onKeyDown}/>
             <button onClick={onClick}>추가</button>
             <ul>{nameList}</ul>
         </>
